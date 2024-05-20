74f7aba2dc3a72aa5632bd73b54782829bb5fd3a
Krishan Walia <krishanw30@gmail.com>
2024-05-20 16:18:03 +0530

    Minor Template Update

--- template.py
+++ template.py
@@ -19,7 +19,7 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
-    "parms.yaml",
+    "params.yaml",
     "app.py",
     "main.py",
     "requirements.txt",
