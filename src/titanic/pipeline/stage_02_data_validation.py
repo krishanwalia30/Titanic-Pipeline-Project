ac495dbf3d340a57009cdba468a6bfd6dd0230bf
Krishan Walia <krishanw30@gmail.com>
2024-05-22 16:11:57 +0530

    Data Validation Pipeline added

---
+++ src/titanic/pipeline/stage_02_data_validation.py
@@ -0,0 +1,13 @@
+from titanic.components.data_validation import DataValidation
+from titanic.config.configuration import ConfigurationManager
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
